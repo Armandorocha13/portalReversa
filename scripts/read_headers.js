ff26df094a4eb0cba8b3b19ace71a6664cad0ac7
Armandorocha13 <mandoqxo@gmail.com>
2026-05-14 15:09:52 -0300

    chore: limpeza completa de automacao e robot

--- scripts/read_headers.js
+++
@@ -1,6 +0,0 @@
-const xlsx = require('xlsx');
-const workbook = xlsx.readFile('FFA - REVERSA_ETER_XXX_BAIXO_GIRO.xlsx');
-const sheetName = workbook.SheetNames[0];
-const sheet = workbook.Sheets[sheetName];
-const data = xlsx.utils.sheet_to_json(sheet, {header: 1});
-console.log(JSON.stringify(data[0]));
